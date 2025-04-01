0c1d0f9e0663f09c486866abc5fe4f09bb4c02a8
ARYAN MASSOUDY <116915753+aryan-massoudy@users.noreply.github.com>
2025-04-01 08:51:36 +0200

    MVT paradigm implemented

--- hello_app/views.py
+++ hello_app/views.py
@@ -1,14 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from hello_app.models import Topic,Webpage,AccessRecord
+
 
 
 # Create your views here.
 
 def hello_world(request):
-    my_dict = {'insert_me': "Hello this comes from the django tempalte tag defined in the view"}
-    return render(request,'hello_app/index.html',context=my_dict)
-
-
-def help(request):
-    helpdict = {'help_insert': 'HELP PAGE'}
-    return render(request,'hello_app/index.html',context=helpdict)
+    webpages_list = AccessRecord.objects.order_by('date')
+    date_dic = {'access_records':webpages_list}
+    return render(request,'hello_app/index.html',context=date_dic)
