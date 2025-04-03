fc632a114a9e9e4cef5a973d21f52d97e05bae7d
ARYAN MASSOUDY <116915753+aryan-massoudy@users.noreply.github.com>
2025-04-03 09:11:38 +0200

    Delete project_two_forms directory

--- project_two_forms/basicforms/basicapp/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-
-class FormName(forms.Form):
-    name = forms.CharField()
-    email = forms.EmailField()
-    text = forms.CharField(widget=forms.Textarea)
-    bot_catcher = forms.CharField(required=False,widget=forms.HiddenInput)
